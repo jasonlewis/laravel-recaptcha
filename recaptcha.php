7d94cfbff2cc2be0a2d1853644a6409d5d238025
Jason Lewis <jason.lewis1991@gmail.com>
2012-03-26 20:03:34 +1100

    Another couple bug fixes. Hopefully should be good now.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- recaptcha.php
+++ recaptcha.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace Recaptcha;
 /*
  * This is a PHP library that handles calling reCAPTCHA.
  *    - Documentation and latest version
--- start.php
+++ start.php
@@ -4,7 +4,7 @@ Autoloader::map(array(
 	'Recaptcha\\Recaptcha' =>  __DIR__ . DS . 'recaptcha' . EXT
 ));
 
-Laravel\Validator::register('validate_recaptcha', function($attribute, $value, $parameters)
+Laravel\Validator::register('recaptcha', function($attribute, $value, $parameters)
 {
 	$recaptcha = Recaptcha\Recaptcha::recaptcha_check_answer($parameters[0], Laravel\Request::ip(), Laravel\Input::get('recaptcha_challenge_field'), $value);
 
