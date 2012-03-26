36a71772de5b8adf5e2c365a5acc3f9bf7c5c49f
Jason Lewis <jason.lewis1991@gmail.com>
2012-03-26 18:45:46 +1100

    Initial commit
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ start.php
@@ -0,0 +1,12 @@
+<?php
+
+Autoloader::map(
+	'Recaptcha\\Recaptcha' =>  __DIR__ . DS . 'recaptcha' . EXT
+);
+
+Laravel\Validator::register('validate_recaptcha', function($attribute, $value, $parameters)
+{
+	$recaptcha = Recaptcha\Recaptcha::recaptcha_check_answer($parameters[0], Laravel\Request::ip(), Laravel\Input::get('recaptcha_challenge_field'), $value);
+
+	return $recaptcha->is_valid;
+});
